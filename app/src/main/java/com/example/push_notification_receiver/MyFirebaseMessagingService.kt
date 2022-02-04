d2459ddacd4159a599cf2aecdfd523aef3924210
hwayeon <hwayeon351@gmail.com>
2022-02-04 22:02:53 +0900

    Feat: Update layout and Connect Firebase for using FCM

---
+++ app/src/main/java/com/example/push_notification_receiver/MyFirebaseMessagingService.kt
@@ -0,0 +1,15 @@
+package com.example.push_notification_receiver
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+    }
+}
